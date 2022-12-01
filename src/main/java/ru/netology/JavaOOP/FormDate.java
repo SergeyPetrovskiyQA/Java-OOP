7e94e374a8344b9c69fe2675e97d107d9c4eb94b
Sergey Petrovskiy <petrovskiy.sa@gmail.com>
2022-12-02 00:29:16 +0300

    add code

---
+++ src/main/java/ru/netology/JavaOOP/FormDate.java
@@ -0,0 +1,13 @@
+package ru.netology.JavaOOP;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+}
+
+
+
+//day": 13,
+//        "month": 6,
+//        "year": 1999
